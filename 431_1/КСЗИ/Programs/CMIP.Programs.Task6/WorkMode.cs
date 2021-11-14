861ff2a4321da510d3868e813ade723ee72e8f09
Завенягин Максим <maximzombi@yandex.ru>
2021-11-14 22:48:58 +0400

    Add CMIP task6

---
+++ 431_1/КСЗИ/Programs/CMIP.Programs.Task6/WorkMode.cs
@@ -0,0 +1,15 @@
+﻿namespace CMIP.Programs.Task6
+{
+    internal enum WorkMode
+    {
+        NONE = 0,
+        ENCRYPT_KEY1 = 1,
+        ENCRYPT_KEY2 = 2,
+        CALCULATE_SUPER_KEY = 3,
+        ENCRYPT_SUPER_KEY = 4,
+        DECRYPT_KEY1 = 5,
+        DECRYPT_KEY2 = 6,
+        DECRYPT_SUPER_KEY = 7,
+        CLOSE_PROGRAM = 0
+    }
+}
--- 431_1/КСЗИ/Programs/СMIP.Common/PathSettings.cs
+++ 431_1/КСЗИ/Programs/СMIP.Common/PathSettings.cs
@@ -5,11 +5,20 @@
         private const string SandBoxFolderPath = @"..\..\..\..\..\..\..\SandBox\КСЗИ";
         public const string BooksPath = SandBoxFolderPath + "\\" + "Книги";
         private const string Task2_FolderPath = SandBoxFolderPath + "\\" + "Задание_2";
+        private const string Task6_FolderPath = SandBoxFolderPath + "\\" + "Задание_6";
 
         public const string Task2_CodedText = Task2_FolderPath + "\\" + "Шифрограмма.txt";
         public const string Task2_DecodedText = Task2_FolderPath + "\\" + "РасшифрованныйТекст.txt";
         public const string Task2_Bigrams = Task2_FolderPath + "\\" + "Биграммы.txt";
         public const string Task2_Frequencies = Task2_FolderPath + "\\" + "Частоты.txt";
         public const string Task2_Alphabet = Task2_FolderPath + "\\" + "Алфавит.txt";
+
+        public const string Task6_Alphabet = Task6_FolderPath + "\\" + "Алфавит.txt";
+        public const string Task6_Key1 = Task6_FolderPath + "\\" + "Ключ1.txt";
+        public const string Task6_Key2 = Task6_FolderPath + "\\" + "Ключ2.txt";
+        public const string Task6_KeyResult = Task6_FolderPath + "\\" + "КлючСуперпозиции.txt";
+        public const string Task6_OpenText = Task6_FolderPath + "\\" + "Текст.txt";
+        public const string Task6_CodedText = Task6_FolderPath + "\\" + "Шифрограмма.txt";
+        public const string Task6_DecodedText = Task6_FolderPath + "\\" + "РасшифрованныйТекст.txt";
     }
 }
