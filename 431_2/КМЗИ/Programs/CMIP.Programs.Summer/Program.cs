68b92ed17698b2fb6ffd51102ce202b2a3fe85ae
Максим Максим <maximzombi@yandex.ru>
2022-02-19 18:04:04 +0400

    CMIP: Add summer of 2 numbers

---
+++ 431_2/КМЗИ/Programs/CMIP.Programs.Summer/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CMIP.Programs.Summer
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var alphabet = new List<char>() { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+            var numbersSystemBase = ReadBaseOfNumberSystem();
+            alphabet = alphabet.Take(numbersSystemBase).ToList();
+
+            var number1 = ReadNumber(alphabet);
+            var number2 = ReadNumber(alphabet);
+
+            var summer = new Summer(alphabet);
+            var result = summer.Summ(number1, number2);
+
+            var outputList = new List<string>
+            {
+                new string(number1),
+                new string(number2),
+                new string('-', Math.Max(Math.Max(number1.Length, number2.Length), result.Length)),
+                new string(result)
+            };
+
+            var padding = outputList.Max(x => x.Length);
+            for (int i = 0; i < outputList.Count; i++)
+            {
+                outputList[i] = outputList[i].PadLeft(padding);
+            }
+            outputList[0] = "+" + outputList[0];
+            outputList[1] = "+" + outputList[1];
+            padding = outputList.Max(x => x.Length);
+            for (int i = 0; i < outputList.Count; i++)
+            {
+                outputList[i] = outputList[i].PadLeft(padding);
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, outputList));
+        }
+
+        private static int ReadBaseOfNumberSystem()
+        {
+            Console.WriteLine("Введите основание системы счисления (число от 2 до 16)...");
+
+            for (; ; )
+            {
+                var input = Console.ReadLine();
+
+                if (!int.TryParse(input, out var result) || result < 2 || result > 16)
+                {
+                    Console.WriteLine("Ожидалось целое число из отрезка [2..16]. Повторите попытку...");
+                    continue;
+                }
+
+                Console.WriteLine();
+                return result;
+            }
+        }
+
+        private static char[] ReadNumber(List<char> alphabet)
+        {
+            Console.WriteLine("Веедите число, состоящее только из символов алфавита...");
+            for (; ; )
+            {
+                var input = Console.ReadLine();
+
+                if (input.Any(x => !alphabet.Contains(x)))
+                {
+                    Console.WriteLine("Число должно состоять только из символов алфавита. Повторите попытку...");
+                    continue;
+                }
+
+                Console.WriteLine();
+                return input.ToArray();
+            }
+        }
+    }
+}
---
+++ 431_2/КМЗИ/Programs/CMIP.Programs.Summer/Summer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CMIP.Programs.Summer
+{
+    internal class Summer
+    {
+        public Summer(List<char> alphabet)
+        {
+            Alphabet = alphabet ?? throw new ArgumentNullException(nameof(alphabet));
+        }
+
+        public List<char> Alphabet { get; set; }
+
+        public char[] Summ(char[] number1, char[] number2)
+        {
+            if (number1.Any(x => !Alphabet.Contains(x)))
+            {
+                throw new ArgumentException("Number can contain anly alphabet symbols.", nameof(number1));
+            }
+
+            if (number2.Any(x => !Alphabet.Contains(x)))
+            {
+                throw new ArgumentException("Number can contain anly alphabet symbols.", nameof(number2));
+            }
+
+            number1 = number1.Reverse().ToArray();
+            number2 = number2.Reverse().ToArray();
+
+            var remains = 0;
+            var result = new List<char>();
+            for (var index = 0; index < number1.Length || index < number2.Length; index++)
+            {
+                var current = remains;
+                if (index < number1.Length)
+                {
+                    current += Alphabet.IndexOf(number1[index]);
+                }
+
+                if (index < number2.Length)
+                {
+                    current += Alphabet.IndexOf(number2[index]);
+                }
+
+                remains = current / Alphabet.Count;
+                result.Add(Alphabet[current % Alphabet.Count]);
+            }
+
+            result.Add(Alphabet[remains]);
+            result.Reverse();
+            return result.SkipWhile(x => x == Alphabet[0]).ToArray();
+        }
+    }
+}
