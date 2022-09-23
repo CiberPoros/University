c919b901bed2d7e5e694209deb3bb3401240ff6f
Максим Максим <maximzombi@yandex.ru>
2022-09-24 00:46:34 +0400

    Add 1st task Neyral nets

---
+++ 531_1/Нейронки/Programs/NeuralNets.Programs.GraphSerializer/Graph.cs
@@ -0,0 +1,132 @@
+﻿namespace NeuralNets.Programs.GraphSerializer
+{
+    public class Graph
+    {
+        public Graph()
+        {
+            Vertexes = new HashSet<Vertex>();
+            Arcs = new List<Arc>();
+        }
+
+        public HashSet<Vertex> Vertexes { get; set; }
+        public List<Arc> Arcs { get; set; }
+
+        public static bool TryCreateByStringDescription(string descriprion, out Graph result)
+        {
+            result = new Graph();
+
+            var splitedArr = descriprion.Split('(', ')').Select(x => x.Trim(' ', ',')).Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+            foreach (var splited in splitedArr)
+            {
+                var arcSplited = splited.Split(',').Select(x => x.Trim(' ')).Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+                if (arcSplited.Length != 3)
+                {
+                    return false;
+                }
+
+                if (!int.TryParse(arcSplited[2], out var order) || order < 0)
+                {
+                    return false;
+                }
+
+                var from = new Vertex(arcSplited[0]);
+                var to = new Vertex(arcSplited[1]);
+
+                if (!result.Vertexes.TryGetValue(from, out var fromOld))
+                {
+                    result.Vertexes.Add(from);
+                }
+                else
+                {
+                    from = fromOld;
+                }
+
+                if (!result.Vertexes.TryGetValue(to, out var toOld))
+                {
+                    result.Vertexes.Add(to);
+                }
+                else
+                {
+                    to = toOld;
+                }
+
+                var arc = new Arc(from, to, order);
+                if (result.Arcs.Contains(arc))
+                {
+                    return false;
+                }
+
+                result.Arcs.Add(arc);
+            }
+
+            result.Arcs = result.Arcs.OrderBy(x => (x.From, x.To, x.Order)).ToList();
+
+            return true;
+        }
+    }
+
+    public class Vertex : IComparable<Vertex>
+    {
+        public string Name { get; set; }
+
+        public Vertex()
+        {
+
+        }
+
+        public Vertex(string name)
+        {
+            Name = name;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Vertex vertex &&
+                   Name == vertex.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name);
+        }
+
+        public int CompareTo(Vertex? other)
+        {
+            return Name.CompareTo(other.Name);
+        }
+    }
+
+    public class Arc
+    {
+        public Arc()
+        {
+
+        }
+
+        public Arc(Vertex from, Vertex to, int order)
+        {
+            From = from;
+            To = to;
+            Order = order;
+        }
+
+        public Vertex From { get; set; }
+        public Vertex To { get; set; }
+        public int Order { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Arc arc &&
+                   EqualityComparer<Vertex>.Default.Equals(From, arc.From) &&
+                   EqualityComparer<Vertex>.Default.Equals(To, arc.To) &&
+                   Order == arc.Order;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(From, To, Order);
+        }
+    }
+}
---
+++ 531_1/Нейронки/Programs/NeuralNets.Programs.GraphSerializer/Program.cs
@@ -0,0 +1,74 @@
+﻿using NeuralNets.Programs.GraphSerializer;
+using System.Text.Json;
+
+internal class Program
+{
+    private string? _input1 = null;
+    private string? _input2 = null;
+    private string? _output1 = null;
+    private string? _output2 = null;
+
+    static void Main(string[] args)
+    {
+        var program = new Program();
+        program.ParseParams(args);
+
+        if (program._input1 is null)
+        {
+            Console.WriteLine("Обязательный параметр input1 не задан!");
+            return;
+        }
+
+        if (program._output1 is null)
+        {
+            Console.WriteLine("Обязательный параметр output1 не задан!");
+            return;
+        }
+
+        var description = File.ReadAllText(program._input1);
+
+        if (!Graph.TryCreateByStringDescription(description, out var graph))
+        {
+            Console.WriteLine($"Ошибка в формате файла {program._input1}");
+            return;
+        }
+
+        string jsonString = JsonSerializer.Serialize(graph, new JsonSerializerOptions() { WriteIndented = true });
+
+        File.WriteAllText(program._output1, jsonString);
+        Console.WriteLine($"Граф сериализован и записан в файл {program._output1}");
+    }
+
+    private void ParseParams(string[] args)
+    {
+        foreach (var arg in args)
+        {
+            var splited = arg.Split('=').Where(x => !string.IsNullOrEmpty(x)).Select(x => x.Trim().ToLower()).ToArray();
+
+            if (splited.Length != 2)
+            {
+                Console.WriteLine($"Неизвестный параметр {arg} был пропущен.");
+                continue;
+            }
+
+            switch (splited[0])
+            {
+                case "input1":
+                    _input1 = splited[1];
+                    break;
+                case "input2":
+                    _input2 = splited[1];
+                    break;
+                case "output1":
+                    _output1 = splited[1];
+                    break;
+                case "output2":
+                    _output2 = splited[1];
+                    break;
+                default:
+                    Console.WriteLine($"Неизвестный параметр {arg} был пропущен.");
+                    continue;
+            }
+        }
+    }
+}
