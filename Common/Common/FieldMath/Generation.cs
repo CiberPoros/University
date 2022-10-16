7221e15f790eba5e6f49848d60d668454c75818d
Максим Максим <maximzombi@yandex.ru>
2022-10-16 19:23:36 +0400

    Add IntF

---
+++ Common/Common/FieldMath/Generation.cs
@@ -0,0 +1,76 @@
+﻿using Common.IsPrimeChecking;
+using System;
+using System.Numerics;
+
+namespace Common.FieldMath
+{
+    public static class Generation
+    {
+        private static readonly Random _random = new();
+
+        public static (BigInteger prime, BigInteger primitiveRoot) GetBigPrimeWithPrimitiveElement(int minBitsCount)
+        {
+            var primeDivider = GetBigPrime(50);
+
+            var degreeOf2 = new BigInteger(1);
+            degreeOf2 <<= Math.Max(50, minBitsCount - 50);
+
+            var checher = new CheckerByRabinMiller() { RoundsCount = 30 };
+            var prime = primeDivider * degreeOf2 + 1;
+            while (!checher.IsPrime(prime))
+            {
+                degreeOf2 <<= 1;
+                prime = primeDivider * degreeOf2 + 1;
+            }
+
+            while (true)
+            {
+                var primitiveRoot = GetRandom(Math.Max(50, minBitsCount - 1));
+
+                if (BigInteger.ModPow(primitiveRoot, 2, prime - 1) == 1)
+                {
+                    continue;
+                }
+
+                if (BigInteger.ModPow(primitiveRoot, primeDivider, prime - 1) == 1)
+                {
+                    continue;
+                }
+
+                return (prime, primitiveRoot);
+            }
+        }
+
+        public static BigInteger GetBigPrime(int minBitsCount)
+        {
+            var checher = new CheckerByRabinMiller() { RoundsCount = 30 };
+            var result = 1;
+
+            for (int i = 0; i < Math.Max(minBitsCount / 2, 25); i++)
+            {
+                result <<= 1;
+                result += _random.Next(2);
+            }
+
+            while (!checher.IsPrime(result))
+            {
+                result += 2;
+            }
+
+            return result;
+        }
+
+        public static BigInteger GetRandom(int bitsCount)
+        {
+            var result = 1;
+
+            for (int i = 0; i < bitsCount; i++)
+            {
+                result <<= 1;
+                result += _random.Next(2);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Common/Common/FieldMath/IntF.cs
@@ -0,0 +1,77 @@
+﻿using System.Numerics;
+
+namespace Common.FieldMath
+{
+    public class IntF
+    {
+        public static BigInteger Modulo { get; set; }
+
+        public BigInteger Value { get; set; }
+
+        public IntF(BigInteger value)
+        {
+            Value = ToField(value);
+        }
+
+        public IntF GetInverse()
+        {
+            var (_, u, _) = GcdExtend(Value, Modulo);
+            return new IntF(u);
+        }
+
+        public static IntF operator +(IntF left, IntF right)
+        {
+            return new IntF(left.Value + right.Value);
+        }
+
+        public static IntF operator -(IntF left, IntF right)
+        {
+            return new IntF(left.Value - right.Value);
+        }
+
+        public static IntF operator *(IntF left, IntF right)
+        {
+            return new IntF(left.Value * right.Value);
+        }
+
+        public static IntF operator /(IntF left, IntF right)
+        {
+            var rightInverse = right.GetInverse();
+
+            return left * rightInverse;
+        }
+
+        private static (BigInteger result, BigInteger u, BigInteger v) GcdExtend(BigInteger x1, BigInteger x2)
+        {
+            x1 = ToField(x1);
+            x2 = ToField(x2);
+
+            BigInteger u = 0, v = 0;
+            var result = ByEuklidInternal(x1, x2, ref u, ref v);
+            return (result, u, v);
+        }
+
+        private static BigInteger ByEuklidInternal(BigInteger x1, BigInteger x2, ref BigInteger u, ref BigInteger v)
+        {
+            if (x1 == 0)
+            {
+                u = 0;
+                v = 1;
+
+                return x2;
+            }
+
+            BigInteger u1 = 0, v1 = 0;
+            BigInteger d = ByEuklidInternal(x2 % x1, x1, ref u1, ref v1);
+            u = v1 - (x2 / x1) * u1;
+            v = u1;
+
+            return d;
+        }
+
+        private static BigInteger ToField(BigInteger value)
+        {
+            return ((value % Modulo) + Modulo) % Modulo;
+        }
+    }
+}
