e34cc26577940c4094ae863b3378379c6cc5eda7
Максим Максим <maximzombi@yandex.ru>
2022-11-03 02:53:36 +0400

    Add Molchanov task2

---
+++ 531_1/Теоретико числовые методы в криптографии/Programs/Molchanov.Programs.LabWork2/F_Int.cs
@@ -0,0 +1,239 @@
+﻿using System.Numerics;
+
+namespace Molchanov.Programs.LabWork2
+{
+    public class F_int
+    {
+        public BigInteger _val;
+        public BigInteger _mod;
+
+        public override string ToString()
+        {
+            return _val.ToString();
+        }
+
+        override public int GetHashCode()
+        {
+            return _val.GetHashCode();
+        }
+
+        override public bool Equals(object val)
+        {
+            if (val.GetType().ToString() != "F_int")
+                return false;
+
+            if ((val as F_int)._val == _val && (val as F_int)._mod == _mod)
+                return true;
+            else
+                return false;
+        }
+
+        public F_int(BigInteger val, BigInteger mod)
+        {
+            _val = val;
+            _mod = mod;
+        }
+
+        public static F_int operator +(F_int a, F_int b)
+        {
+            return new F_int((((a._val + b._val) % a._mod) + a._mod) % a._mod, a._mod);
+        }
+
+        public static F_int operator -(F_int a, F_int b)
+        {
+            return new F_int((((a._val - b._val) % a._mod) + a._mod) % a._mod, a._mod);
+        }
+
+        public static F_int operator -(F_int a)
+        {
+            return new F_int((((a._val) % a._mod) + a._mod) % a._mod, a._mod);
+        }
+
+        public static F_int operator *(F_int a, F_int b)
+        {
+            return new F_int((((a._val * b._val) % a._mod) + a._mod) % a._mod, a._mod);
+        }
+
+        public static F_int operator *(int a, F_int b)
+        {
+            return new F_int((((a * b._val) % b._mod) + b._mod) % b._mod, b._mod);
+        }
+
+        public static F_int operator *(BigInteger a, F_int b)
+        {
+            return new F_int((((a * b._val) % b._mod) + b._mod) % b._mod, b._mod);
+        }
+
+        public static F_int operator *(F_int a, int b)
+        {
+            return new F_int((((a._val * b) % a._mod) + a._mod) % a._mod, a._mod);
+        }
+
+        public static F_int operator /(F_int a, F_int b)
+        {
+            F_int b_obr = GetInverseElement(b);
+            return new F_int((((a._val * b_obr._val) % a._mod) + a._mod) % a._mod, a._mod);
+        }
+
+        public static bool operator <(F_int a, F_int b)
+        {
+            return a._val < b._val;
+        }
+
+        public static bool operator >(F_int a, F_int b)
+        {
+            return a._val > b._val;
+        }
+
+        public static bool operator !=(F_int a, int b)
+        {
+            return a._val != b;
+        }
+
+        public static bool operator ==(F_int a, int b)
+        {
+            return a._val == b;
+        }
+
+        public static bool operator !=(F_int a, F_int b)
+        {
+            return a._val != b._val;
+        }
+
+        public static bool operator ==(F_int a, F_int b)
+        {
+            return a._val == b._val;
+        }
+
+        /// <summary>
+        /// Находит обратный элемент в поле, вернет -1, если обратного нет
+        /// </summary>      
+        /// <returns></returns>
+        public static F_int GetInverseElement(F_int val)
+        {
+            BigInteger x = 0, y = 0;
+            GcdAdvanced(val._val, val._mod, ref x, ref y);
+            x = ((x % val._mod) + val._mod) % val._mod;
+            return new F_int(x, val._mod);
+        }
+
+        /// <summary>
+        /// Вычисляет квадратный корень из числа val по алгоритму Шенкса за O(log^3(a))
+        /// </summary>      
+        /// <returns></returns>
+        public static F_int Sqrt(F_int a)
+        {
+            F_int x = new F_int(0, a._mod);
+
+            // step 1
+            if (a._val % 4 == 3)
+            {
+                x._val = BigInteger.ModPow(a._val, (a._mod + 1) / 4, a._mod);
+                return x;
+            }
+
+            // step 2
+            BigInteger s = a._mod - 1;
+            BigInteger alpha = 0;
+
+            while (s % 2 == 0)
+            {
+                alpha++;
+                s >>= 1;
+            }
+
+            if (alpha == 1)
+            {
+                F_int answerr = new F_int(BigInteger.ModPow(a._val, (a._mod + 1) / 4, a._mod), a._mod);
+                if (answerr._mod - answerr._val < answerr._val)
+                    answerr._val = answerr._mod - answerr._val;
+                return answerr;
+            }
+
+            // step 3
+            BigInteger n = -1;
+            for (BigInteger i = 2; i < a._mod; i++)
+            {
+                if (!IsQuadraticResidue(new F_int(i, a._mod)))
+                {
+                    n = i;
+                    break;
+                }
+            }
+
+            // step 4
+            F_int b = new F_int(BigInteger.ModPow(n, s, a._mod), a._mod);
+
+            // step 5
+            F_int r = new F_int(BigInteger.ModPow(a._val, (s + 1) / 2, a._mod), a._mod);
+
+            F_int a_obr = GetInverseElement(a);
+            List<F_int> j = new List<F_int>();
+
+            // 2^ (alpha - 2)
+            BigInteger buffer = BigInteger.Pow(2, (int)alpha - 2);
+
+            if (BigInteger.ModPow((r * r * a_obr)._val, buffer, a._mod) == 1)
+                j.Add(new F_int(0, a._mod));
+            else
+                j.Add(new F_int(1, a._mod));
+
+            F_int summ_j = new F_int(j[0]._val, a._mod);
+
+            BigInteger degree_2 = 1;
+            for (int k = 1; k < alpha - 1; k++)
+            {
+                buffer /= 2;
+
+                F_int value = new F_int(BigInteger.ModPow(b._val, summ_j._val, a._mod), a._mod) * r;
+                value = value * value * a_obr;
+                value = new F_int(BigInteger.ModPow(value._val, buffer, a._mod), a._mod);
+
+                if (value._val == 1)
+                    j.Add(new F_int(0, a._mod));
+                else
+                    j.Add(new F_int(1, a._mod));
+
+                degree_2 <<= 1;
+                summ_j = summ_j + new F_int(j[k]._val, a._mod) * new F_int(degree_2, a._mod);
+            }
+
+            F_int answer = new F_int(BigInteger.ModPow(b._val, summ_j._val, a._mod), a._mod) * r;
+            if (answer._mod - answer._val < answer._val)
+                answer._val = answer._mod - answer._val;
+            return answer;
+        }
+
+        /// <summary>
+        /// Проверяет, является ли число квадратичным вычетом по простому модулю. На основе критерия Эйлера
+        /// </summary>      
+        /// <returns> True, если число является квадратичным вычетом </returns>
+        public static bool IsQuadraticResidue(F_int a)
+        {
+            BigInteger val = BigInteger.ModPow(a._val, (a._mod - 1) / 2, a._mod);
+            if (val == 1)
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Расширенный алгоритм Евклида
+        /// </summary>
+        /// <returns></returns>
+        private static BigInteger GcdAdvanced(BigInteger a, BigInteger b, ref BigInteger x, ref BigInteger y)
+        {
+            if (a == 0)
+            {
+                x = 0; y = 1;
+                return b;
+            }
+
+            BigInteger x1 = 0, y1 = 0;
+            BigInteger d = GcdAdvanced(b % a, a, ref x1, ref y1);
+            x = y1 - (b / a) * x1;
+            y = x1;
+            return d;
+        }
+    }
+}
---
+++ 531_1/Теоретико числовые методы в криптографии/Programs/Molchanov.Programs.LabWork2/Program.cs
@@ -0,0 +1,186 @@
+﻿using Common;
+using System.Numerics;
+
+namespace Molchanov.Programs.LabWork2;
+
+internal class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            var task = ReadTaskNumber();
+
+            switch (task)
+            {
+                case TaskNumber.АлгоритмРазложенияВЦепнуюДробь:
+                    HandleTask1();
+                    break;
+                case TaskNumber.АлгоритмВычисленияСимволаЯкоби:
+                    HandleTask3();
+                    break;
+                case TaskNumber.АлгоритмВычисленияСимволаЛежандра:
+                    HandleTask4();
+                    break;
+                case TaskNumber.ИзвленениеКвадратногоКорняВКольцеВычетов:
+                    HandleTask5();
+                    break;
+                case TaskNumber.None:
+                    return;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private static void HandleTask1()
+    {
+        var (a, b) = ReadPairBigIntegers("делитель и делимое");
+        Console.WriteLine($"Результат: {Task1(a, b)}");
+    }
+
+    private static void HandleTask3()
+    {
+        var (a, b) = ReadPairBigIntegers("2 числа a и b");
+        Console.WriteLine($"Результат: {Task3(a, b)}");
+    }
+
+    private static void HandleTask4()
+    {
+        var (a, b) = ReadPairBigIntegers("2 числа a и b");
+        Console.WriteLine($"Результат: {Task4(a, b)}");
+    }
+
+    private static void HandleTask5()
+    {
+        var (a, b) = ReadPairBigIntegers("2 числа a и p");
+        Console.WriteLine($"Результат: {Task5(a, b)}");
+    }
+
+    private static string Task1(BigInteger a, BigInteger b)
+    {
+        var list = new List<BigInteger>();
+        Gcd(list, a, b);
+
+        return $"[{list[0]};{string.Join(",", list.Skip(1))}]";
+    }
+
+    private static BigInteger Task3(BigInteger a, BigInteger b)
+    {
+        return Jacobian.Calculate(a, b);
+    }
+
+    private static BigInteger Task4(BigInteger a, BigInteger b)
+    {
+        var res = BigInteger.ModPow(a, (b - 1) / 2, b);
+        if (res == b - 1)
+            res = -1;
+
+        return res;
+    }
+
+    private static BigInteger Task5(BigInteger a, BigInteger p)
+    {
+        var fInt = new F_int(a, p);
+
+        var sqrt = F_int.Sqrt(fInt);
+
+        return sqrt._val;
+    }
+
+    private static BigInteger Gcd(List<BigInteger> list, BigInteger a, BigInteger b)
+    {
+        if (b > 0)
+            list.Add(a / b);
+
+        return b > 0 ? Gcd(list, b, a % b) : a;
+    }
+
+    private static (BigInteger val1, BigInteger val2) ReadPairBigIntegers(string addInfo = "")
+    {
+        Console.WriteLine($"Введите 2 целых положительных числа через пробел {addInfo}:");
+        while (true)
+        {
+            var input = Console.ReadLine()!;
+            var splited = input.Split().Where(s => !string.IsNullOrEmpty(s)).ToArray();
+
+            if (splited.Length != 2)
+            {
+                Console.WriteLine("Ошибка формата входных данных. Повторите попытку...");
+                continue;
+            }
+
+            if (!BigInteger.TryParse(splited[0], out var val1))
+            {
+                Console.WriteLine("Ошибка формата входных данных. Повторите попытку...");
+                continue;
+            }
+
+            if (!BigInteger.TryParse(splited[1], out var val2))
+            {
+                Console.WriteLine("Ошибка формата входных данных. Повторите попытку...");
+                continue;
+            }
+
+            if (val1 <= 0 || val2 <= 0)
+            {
+                Console.WriteLine("Ошибка формата входных данных. Повторите попытку...");
+                continue;
+            }
+
+            Console.WriteLine();
+            return (val1, val2);
+        }
+    }
+
+    private static TaskNumber ReadTaskNumber()
+    {
+        Console.WriteLine("Выберите номер задания: ");
+        Console.WriteLine("1. Алгоритм разложения в цепную дробь");
+        Console.WriteLine("2. Алгоритм приложений цепных дробей");
+        Console.WriteLine("3. Алгоритм вычисления символа якоби");
+        Console.WriteLine("4. Алгоритм вычисления символа лагранжа");
+        Console.WriteLine("5. Извленение квадратного корня в кольце вычетов");
+        Console.WriteLine("0. Закрыть программу");
+        Console.WriteLine();
+
+        while (true)
+        {
+            var key = Console.ReadKey(true).Key;
+
+            switch (key)
+            {
+                case ConsoleKey.NumPad1:
+                case ConsoleKey.D1:
+                    return TaskNumber.АлгоритмРазложенияВЦепнуюДробь;
+                case ConsoleKey.NumPad2:
+                case ConsoleKey.D2:
+                    return TaskNumber.АлгоритмПриложенийЦепныхДробей;
+                case ConsoleKey.NumPad3:
+                case ConsoleKey.D3:
+                    return TaskNumber.АлгоритмВычисленияСимволаЯкоби;
+                case ConsoleKey.NumPad4:
+                case ConsoleKey.D4:
+                    return TaskNumber.АлгоритмВычисленияСимволаЛежандра;
+                case ConsoleKey.NumPad5:
+                case ConsoleKey.D5:
+                    return TaskNumber.ИзвленениеКвадратногоКорняВКольцеВычетов;
+                case ConsoleKey.NumPad0:
+                case ConsoleKey.D0:
+                    return TaskNumber.None;
+                default:
+                    continue;
+            }
+        }
+    }
+
+    public enum TaskNumber
+    {
+        None = 0,
+        АлгоритмРазложенияВЦепнуюДробь = 1,
+        АлгоритмПриложенийЦепныхДробей = 2,
+        АлгоритмВычисленияСимволаЯкоби = 3,
+        АлгоритмВычисленияСимволаЛежандра = 4,
+        ИзвленениеКвадратногоКорняВКольцеВычетов = 5
+    }
+}
