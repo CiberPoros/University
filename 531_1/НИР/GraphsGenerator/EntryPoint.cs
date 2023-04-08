3fe7806414ff3663c8e1ad0970c064449cb57de9
Максим Максим <maximzombi@yandex.ru>
2023-04-09 01:31:51 +0400

    Increase currentNumber

--- 531_1/НИР/GraphsGenerator/EntryPoint.cs
+++ 531_1/НИР/GraphsGenerator/EntryPoint.cs
@@ -50,6 +50,7 @@ namespace GraphsGenerator
 
                         if (skipPartition)
                         {
+                            currentNumber++;
                             continue;
                         }
 
